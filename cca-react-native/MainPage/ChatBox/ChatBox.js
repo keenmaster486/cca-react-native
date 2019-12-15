fb8fabf7cc467fcccae78bf70e9be08ed90ca4ef
keenmaster486 <cebrock106@gmail.com>
2019-12-14 20:44:45 -0700

    major updates to image functionality

--- cca-react-native/MainPage/ChatBox/ChatBox.js
+++ cca-react-native/MainPage/ChatBox/ChatBox.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { useRef, StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList, TouchableOpacity, KeyboardAvoidingView} from 'react-native';
+import { useRef, StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList, RefreshControl, TouchableOpacity, KeyboardAvoidingView, Image} from 'react-native';
+import CameraRoll from "@react-native-community/cameraroll";
 
 import { AutoGrowingTextInput } from 'react-native-autogrow-textinput';
+
+import * as ImagePicker from 'expo-image-picker';
+
+import * as MediaLibrary from 'expo-media-library';
 
 
 const styles = StyleSheet.create({
@@ -11,7 +16,6 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		// justifyContent: 'center',
 		padding: 20
-		//minHeight: '100%'
 	},
 	topContainer: {
 		flexDirection: 'row',
@@ -40,7 +44,8 @@ const styles = StyleSheet.create({
 	textbox:
 	{
 		maxHeight: 200,
-		width: '95%',
+		width: '90%',
+		marginBottom: 5,
 		borderColor: 'gray',
 		borderWidth: 1,
 		borderRadius: 10,
@@ -73,7 +78,8 @@ const styles = StyleSheet.create({
 	},
 	textInputContainer: {
 		flexDirection: 'row',
-		margin: 5
+		margin: 5,
+		marginBottom: 0
 	},
 	singleMsgContainer: {
 
@@ -141,8 +147,14 @@ class ChatBox extends React.Component
 				name: '',
 				type: ''
 			},
-			msgText: ''
+			msgText: '',
+			uploadingImage: false,
+			imgPreview: false,
+			imgPreviewUri: '',
+			firstLoad: true,
+			messagesBelow: false
 		};
+		this.refreshing = false;
 		//this.flatListRef = useRef;
 	}
 
@@ -157,6 +169,7 @@ class ChatBox extends React.Component
 		});
 
 		this.getUserInfo();
+		this.refs.scrollBoxRef.scrollToEnd();
 	}
 	componentWillUnmount()
 	{
@@ -219,7 +232,7 @@ class ChatBox extends React.Component
 			}
 		});
 		const msgLength = await groupInfo.msgLength;
-		if (oldMsgLength != msgLength)
+		if (oldMsgLength != msgLength || groupInfo.userMetaData.whetherChanged)
 		{
 			await this.getMessages();
 		}
@@ -227,11 +240,13 @@ class ChatBox extends React.Component
 		//this.refs.scrollBoxRef.scrollToEnd();
 		//this.flatListRef.scrollToOffset({offset: 500})
 		//setTimeout(() => this.flatListRef.scrollToEnd(), 200)
+
+		//this.refs.scrollBoxRef.scrollToEnd();
 	}
 
 
 
-	getMessages = async (howMany = 25) =>
+	getMessages = async (howMany = 25, patchInPrevious = false) =>
 	{
 		//gets the most recent messages from the Express API!!!!
 		//and then stores them in the state's messages array
@@ -249,6 +264,17 @@ class ChatBox extends React.Component
 
 
 		let endmsg = msgLength;
+
+		if (patchInPrevious)
+		{
+			endmsg = msgLength - this.state.messages.length;
+			if (endmsg < 0)
+			{
+				//something is horribly wrong
+				console.log("WHAT");
+				return;
+			}
+		}
 
 		const submitURL = await this.props.apiURL + '/groups/' + await this.state.currentGroup.id + '/messages/' + await startmsg + '/' + await endmsg;
 
@@ -272,22 +298,68 @@ class ChatBox extends React.Component
 		{
 			tempMsgs.push(
 			{
-				key: i.toString(),
+				key: (i + (msgLength - howMany)).toString(),
 				userdisplayname: recentMsgs[i].userdisplayname,
 				text: recentMsgs[i].text,
 				image: recentMsgs[i].image,
 				userId: recentMsgs[i].userId
 			});
 		}
-
-		this.setState(
+		
+		if (patchInPrevious)
 		{
-			messages: tempMsgs
-		});
+			this.setState(
+			{
+				messages: [...tempMsgs, ...this.state.messages]
+			});
+		}
+		else
+		{
+			this.setState(
+			{
+				messages: tempMsgs,
+				uploadingImage: false
+			});
+		}
 
-		//this.refs.scrollBoxRef.scrollToEnd();
+		//on first load, scroll to bottom:
+		if (this.state.firstLoad == true)
+		{
+			setTimeout(()=>this.refs.scrollBoxRef.scrollToEnd(),500)
+			this.setState(
+			{
+				firstLoad: false
+			});
+		}
+		else
+		{
+			// this.setState(
+			// {
+			// 	messagesBelow: true
+			// });
+		}
+		this.refreshing = false;
+		//onContentSizeChange={()=>this.refs.scrollBoxRef.scrollToEnd()}
 	}
 
+
+	uploadImgAPICall = async (newMsg) =>
+	{
+		const submitURL = this.props.apiURL + '/groups/' + this.state.currentGroup.id + '/messages/uploadImage';
+
+		let msgResponse = await fetch(submitURL, {
+			method: 'POST',
+			body: JSON.stringify(newMsg),
+			headers:
+		    {
+		    	"Content-Type": "application/json",
+		    	"Authentication": this.props.sessionId
+		    }
+		});
+		msgResponse = await msgResponse.json();
+		console.log(msgResponse);
+		return msgResponse.id;
+	}
 
 
 	addMsgAPICall = async (newMsg) =>
@@ -317,7 +389,7 @@ class ChatBox extends React.Component
 		
 		console.log("Attempting to add new message with text of " + this.state.msgText);
 
-		if (this.state.msgText === '' && this.state.msgImage == '')
+		if (this.state.msgText == '')
 		{
 			console.log("Message is empty; aborting...");
 			return;
@@ -381,7 +453,81 @@ class ChatBox extends React.Component
 
 	handleSendButton = () =>
 	{
-		this.addMsg();
+		if (this.state.msgText != '') {this.addMsg();}
+	}
+
+	handleSelectImages = async () =>
+	{
+		const response = await ImagePicker.launchImageLibraryAsync(
+		{
+			allowsMultipleSelection: true,
+			base64: true
+		});
+		console.log(await response);
+		if (await response.cancelled)
+		{
+			//console.log('cancelled');
+			return;
+		}
+		else
+		{
+			//console.log("NOT CANCELLED");
+			//return;
+			this.setState(
+			{
+				uploadingImage: true
+			});
+			this.refs.scrollBoxRef.scrollToEnd()
+			this.uploadImgAPICall(
+			{
+				userId: this.props.userId,
+				userdisplayname: this.state.displayname,
+				text: '',
+				image: 'data:image/jpeg;base64,' + await response.base64,
+				video: '',
+				url: '',
+				id: ''
+			});
+		}
+	}
+
+	turnOffImgPreview = () =>
+	{
+		this.setState(
+		{
+			imgPreview: false,
+			imgPreviewUri: ''
+		});
+	}
+
+	turnOnImgPreview = (imgURI) =>
+	{
+		this.setState(
+		{
+			imgPreview: true,
+			imgPreviewUri: imgURI
+		});
+	}
+
+	scrollDown = () =>
+	{
+		this.setState(
+		{
+			messagesBelow: false
+		});
+		this.refs.scrollBoxRef.scrollToEnd()
+	}
+
+	getMoreMsgs = () =>
+	{
+		this.refreshing = true;
+		console.log("getMoreMsgs");
+		this.getMessages(this.state.messages.length + 5, true);
+	}
+
+	saveImgPreview = () =>
+	{
+		MediaLibrary.saveToLibraryAsync(this.state.imgPreviewUri);
 	}
 
 	msgTextToDisplay = (item) =>
@@ -407,7 +553,6 @@ class ChatBox extends React.Component
 	{
 		return(
 			<View>
-				
 				<KeyboardAvoidingView style={styles.container} behavior="height">
 					<View style={styles.topContainer}>
 						<TouchableOpacity style={styles.backButton} onPress={this.props.exitChat}><Text style={styles.backButtonText}>{"<"}</Text></TouchableOpacity>
@@ -417,13 +562,25 @@ class ChatBox extends React.Component
 							<Text style={styles.h2} numberOfLines={1}>{this.state.currentGroup.name}</Text>
 						}
 					</View>
-					<ScrollView ref='scrollBoxRef' style={styles.scrollbox}>
+					{this.state.imgPreview ?
+						<View style={{width: '100%', height: '100%', padding: 0, alignItems: 'center'}}>
+							<Image style={{width: 300, height: 400, margin: -10, resizeMode: 'contain'}} source={{uri: this.state.imgPreviewUri}}/>
+							<View style={{flexDirection: 'row', alignItems: 'center', marginTop: 15}}>
+								<Button title="Done" style={{margin: 15, color: 'blue', fontSize: 14}} onPress={this.turnOffImgPreview}></Button>
+								<Button title="Save Image" style={{margin: 15, color: 'blue', fontSize: 14}} onPress={this.saveImgPreview}></Button>
+							</View>
+						</View> : null
+					}
+					<ScrollView
+					refreshControl = {
+						<RefreshControl refreshing={this.refreshing} onRefresh={this.getMoreMsgs}></RefreshControl>
+					}
+					ref='scrollBoxRef' style={styles.scrollbox}>
 						{this.state.currentGroup.id != '' ?
 							(
 							this.state.messages.length != 0 ?
 								<FlatList
 									
-									onContentSizeChange={()=>this.refs.scrollBoxRef.scrollToEnd()}
 									data={this.state.messages}
 									renderItem=
 									{
@@ -433,7 +590,19 @@ class ChatBox extends React.Component
 											{
 												return(
 													<View style={[styles.singleMsgContainer, styles.yourMsgContainer]}>
-														<View style={[styles.singleMsgText, styles.yourMsgText]}>{this.msgTextToDisplay(item)}</View>
+														<View style={[styles.singleMsgText, styles.yourMsgText]}>
+															{this.msgTextToDisplay(item)}
+															{item.image && item.image != '' ?
+																item.image == 'loading' ?
+																	<Image style={{width: 150, height: 150, resizeMode: 'cover'}} source={{uri: this.props.apiURL + '/images/loading.gif'}}/>
+																	:
+																	<TouchableOpacity onPress={this.turnOnImgPreview.bind(null, item.image)}>
+																		<Image style={{width: 150, height: 150, resizeMode: 'cover'}} source={{uri: item.image}}/>
+																	</TouchableOpacity>
+																:
+																null
+															}
+														</View>
 													</View>
 												);
 											}
@@ -441,7 +610,20 @@ class ChatBox extends React.Component
 											{
 												return(
 													<View style={[styles.singleMsgContainer, styles.otherMsgContainer]}>
-														<View style={[styles.singleMsgText, styles.otherMsgText]}>{this.msgTextToDisplay(item)}</View>
+														<View style={[styles.singleMsgText, styles.otherMsgText]}>
+															{this.msgTextToDisplay(item)}
+															{item.image && item.image != '' ?
+																item.image == 'loading' ?
+																	<Image style={{width: 150, height: 150, resizeMode: 'cover'}} source={{uri: this.props.apiURL + '/images/loading.gif'}}/>
+																	:
+																	<TouchableOpacity onPress={this.turnOnImgPreview.bind(null, item.image)}>
+																		<Image style={{width: 150, height: 150, resizeMode: 'cover'}} source={{uri: item.image}}/>
+																	</TouchableOpacity>
+																:
+																null
+															}
+														</View>
+														
 													</View>
 												);
 											}
@@ -454,13 +636,25 @@ class ChatBox extends React.Component
 						:
 							<Text>Getting messages...</Text>
 						}
+						{this.state.uploadingImage ?
+							<View style={[styles.singleMsgContainer, styles.yourMsgContainer]}>
+								<View style={[styles.singleMsgText, styles.yourMsgText]}>
+									<Image style={{width: 150, height: 150, resizeMode: 'cover'}} source={{uri: this.props.apiURL + '/images/loading.gif'}}/>
+								</View>
+							</View> : null
+						}
 						<Text style={styles.h3}></Text>
 						<Text style={styles.h3}></Text>
 					</ScrollView>
-
+					{this.state.messagesBelow ?
+						<TouchableOpacity onPress={this.scrollDown}>
+							<Text>NEW MSGS BELOW</Text>
+						</TouchableOpacity> : null
+					}
 					<View style={styles.textInputContainer}>
 						<AutoGrowingTextInput style={styles.textbox} onChangeText={this.handleChangeText} value={this.state.msgText}></AutoGrowingTextInput>
-						<Button style={styles.button} title='Send' onPress={this.handleSendButton}></Button>
+						<Button style={styles.button} title='=>' onPress={this.handleSendButton}></Button>
+						<Button style={styles.button} title='+' onPress={this.handleSelectImages}></Button>
 					</View>
 				</KeyboardAvoidingView>
 			</View>
